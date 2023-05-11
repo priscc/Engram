d5f29525d3df57836a46714f767705af0967a47b
Priscilla Cancar <priscillacancar@priscillas-air.myfiosgateway.com>
2023-05-11 11:57:36 -0400

    fix trends to now pull a db field type string instead of a field type array

--- src/components/Trends.vue
+++ src/components/Trends.vue
@@ -6,67 +6,55 @@
       </b-row>
       <!-- Social -->
       <b-row v-if="social.length > 0">
-        <b-col >
+        <b-col>
           <div class="header-1" style="color: #FF9800">Social</div>
-          <p v-for="(i, index) in social[0].trends" :key="index" class="text">
-            {{ i }}
+          <p v-for="(trend, index) in social" :key="index" class="text">
+            {{index + 1}}) {{ trend.trend }}
           </p>
         </b-col>
       </b-row>
       <!-- Economic -->
       <b-row v-if="economic.length > 0">
-        <b-col >
+        <b-col>
           <div class="header-1" style="color: #16a175">Economic</div>
-          <p v-for="(i, index) in economic[0].trends" :key="index" class="text">
-            {{ i }}
+          <p v-for="(trend, index) in economic" :key="index" class="text">
+            {{index + 1}}) {{ trend.trend }}
           </p>
         </b-col>
       </b-row>
       <!-- Technology -->
       <b-row v-if="technology.length > 0">
-        <b-col >
+        <b-col>
           <div class="header-1" style="color: #009688">Technology</div>
-          <p
-            v-for="(i, index) in technology[0].trends"
-            :key="index"
-            class="text"
-          >
-            {{ i }}
+          <p v-for="(trend, index) in technology" :key="index" class="text">
+            {{index + 1}}) {{ trend.trend }}
           </p>
         </b-col>
       </b-row>
       <!-- Government -->
       <b-row v-if="government.length > 0">
-        <b-col >
+        <b-col>
           <div class="header-1" style="color: #673AB7">Government</div>
-          <p
-            v-for="(i, index) in government[0].trends"
-            :key="index"
-            class="text"
-          >
-            {{ i }}
+          <p v-for="(trend, index) in government" :key="index" class="text">
+            {{index + 1}}) {{ trend.trend }}
           </p>
         </b-col>
       </b-row>
       <!-- Culture -->
       <b-row v-if="culture.length > 0">
-        <b-col >
+        <b-col>
           <div class="header-1" style="color: #3b4da6">Culture</div>
-          <p v-for="(i, index) in culture[0].trends" :key="index" class="text">
-            {{ i }}
+          <p v-for="(trend, index) in culture" :key="index" class="text">
+            {{index + 1}}) {{ trend.trend }}
           </p>
         </b-col>
       </b-row>
       <!-- Environment -->
       <b-row v-if="environment.length > 0">
-        <b-col >
+        <b-col>
           <div class="header-1" style="color: #b377ff">Environment</div>
-          <p
-            v-for="(i, index) in environment[0].trends"
-            :key="index"
-            class="text"
-          >
-            {{ i }}
+          <p v-for="(trend, index) in environment" :key="index" class="text">
+            {{index + 1}}) {{ trend.trend }}
           </p>
         </b-col>
       </b-row>
@@ -84,30 +72,30 @@ export default {
   components: { comingsoon },
   computed: {
     social() {
-      return storeTopic.state.trends.filter(i => i.type == "society");
+      return storeTopic.state.trends.filter((i) => i.type == "society");
     },
     economic() {
-      return storeTopic.state.trends.filter(i => i.type == "economic");
+      return storeTopic.state.trends.filter((i) => i.type == "economic");
     },
     technology() {
-      return storeTopic.state.trends.filter(i => i.type == "technology");
+      return storeTopic.state.trends.filter((i) => i.type == "technology");
     },
     government() {
-      return storeTopic.state.trends.filter(i => i.type == "politics");
+      return storeTopic.state.trends.filter((i) => i.type == "politics");
     },
     culture() {
-      return storeTopic.state.trends.filter(i => i.type == "culture");
+      return storeTopic.state.trends.filter((i) => i.type == "culture");
     },
     environment() {
-      return storeTopic.state.trends.filter(i => i.type == "environment");
+      return storeTopic.state.trends.filter((i) => i.type == "environment");
     },
     comingSoon() {
       return storeTopic.state.trends.length;
-    }
+    },
   },
   mounted() {
     storeTopic.dispatch("setTopicTrends", this.$route.params.topic);
-  }
+  },
 };
 </script>
 
