3d4da0d634c08390152b8cb40092f02413a85b62
SpiderWolf6 <sohamhomework@gmail.com>
2023-07-06 21:06:01 -0400

    UI Trend Changes

--- src/components/Trends.vue
+++ src/components/Trends.vue
@@ -2,60 +2,66 @@
   <div id="Trends">
     <b-container fluid>
       <b-row>
-        <div class="title">Trends</div>
-      </b-row>
-      <!-- Social -->
-      <b-row v-if="social.length > 0">
-        <b-col>
-          <div class="header-1" style="color: #FF9800">Society</div>
-          <p v-for="(trend, index) in society" :key="index" class="text">
-            {{index + 1}}) {{ trend.trend }}
-          </p>
+        <div class="title">Change and Continuity</div>
+        <b-col class="border-end">
+          <div class="trend_header" style="color: #000000">Change</div>
+          <!-- Social -->
+          <b-row v-if="society.length > 0">
+            <b-col>
+              <div class="trend_header" style="color: #FF9800">Society</div>
+              <p v-for="(trend, index) in society" :key="index" class="trend_text">
+                {{index + 1}}) {{ trend.trend }}
+              </p>
+            </b-col>
+          </b-row>
+          <!-- Economic -->
+          <b-row v-if="economic.length > 0">
+            <b-col>
+              <div class="trend_header" style="color: #16a175">Economic</div>
+              <p v-for="(trend, index) in economic" :key="index" class="trend_text">
+                {{index + 1}}) {{ trend.trend }}
+              </p>
+            </b-col>
+          </b-row>
+          <!-- Technology -->
+          <b-row v-if="technology.length > 0">
+            <b-col>
+              <div class="trend_header" style="color: #009688">Technology</div>
+              <p v-for="(trend, index) in technology" :key="index" class="trend_text">
+                {{index + 1}}) {{ trend.trend }}
+              </p>
+            </b-col>
+          </b-row>
+          <!-- Government -->
+          <b-row v-if="politics.length > 0">
+            <b-col>
+              <div class="trend_header" style="color: #673AB7">Political</div>
+              <p v-for="(trend, index) in politics" :key="index" class="trend_text">
+                {{index + 1}}) {{ trend.trend }}
+              </p>
+            </b-col>
+          </b-row>
+          <!-- Culture -->
+          <b-row v-if="culture.length > 0">
+            <b-col>
+              <div class="trend_header" style="color: #3b4da6">Culture</div>
+              <p v-for="(trend, index) in culture" :key="index" class="trend_text">
+                {{index + 1}}) {{ trend.trend }}
+              </p>
+            </b-col>
+          </b-row>
+          <!-- Environment -->
+          <b-row v-if="environment.length > 0">
+            <b-col>
+              <div class="trend_header" style="color: #b377ff">Environment</div>
+              <p v-for="(trend, index) in environment" :key="index" class="trend_text">
+                {{index + 1}}) {{ trend.trend }}
+              </p>
+            </b-col>
+          </b-row>
         </b-col>
-      </b-row>
-      <!-- Economic -->
-      <b-row v-if="economic.length > 0">
         <b-col>
-          <div class="header-1" style="color: #16a175">Economic</div>
-          <p v-for="(trend, index) in economic" :key="index" class="text">
-            {{index + 1}}) {{ trend.trend }}
-          </p>
-        </b-col>
-      </b-row>
-      <!-- Technology -->
-      <b-row v-if="technology.length > 0">
-        <b-col>
-          <div class="header-1" style="color: #009688">Technology</div>
-          <p v-for="(trend, index) in technology" :key="index" class="text">
-            {{index + 1}}) {{ trend.trend }}
-          </p>
-        </b-col>
-      </b-row>
-      <!-- Government -->
-      <b-row v-if="government.length > 0">
-        <b-col>
-          <div class="header-1" style="color: #673AB7">Political</div>
-          <p v-for="(trend, index) in politics" :key="index" class="text">
-            {{index + 1}}) {{ trend.trend }}
-          </p>
-        </b-col>
-      </b-row>
-      <!-- Culture -->
-      <b-row v-if="culture.length > 0">
-        <b-col>
-          <div class="header-1" style="color: #3b4da6">Culture</div>
-          <p v-for="(trend, index) in culture" :key="index" class="text">
-            {{index + 1}}) {{ trend.trend }}
-          </p>
-        </b-col>
-      </b-row>
-      <!-- Environment -->
-      <b-row v-if="environment.length > 0">
-        <b-col>
-          <div class="header-1" style="color: #b377ff">Environment</div>
-          <p v-for="(trend, index) in environment" :key="index" class="text">
-            {{index + 1}}) {{ trend.trend }}
-          </p>
+          <div class="trend_header" style="color: #000000">Continuity</div>
         </b-col>
       </b-row>
     </b-container>
@@ -72,6 +78,7 @@ export default {
   components: { comingsoon },
   computed: {
     society() {
+      console.log("test", storeTopic.state.trends);
       return storeTopic.state.trends.filter((i) => i.type == "society");
     },
     economic() {
